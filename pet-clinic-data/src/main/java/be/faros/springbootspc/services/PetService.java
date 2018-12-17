aa66e058fc35ffe267b6a04d0489fb701b803d27
Lennart Cockx <lennart.cockx@gmail.com>
2018-12-17 16:27:55 +0100

    Updated git commits to correctly show author

--- pet-clinic-data/src/main/java/be/faros/springbootspc/services/PetService.java
+++ pet-clinic-data/src/main/java/be/faros/springbootspc/services/PetService.java
@@ -1,10 +1,10 @@
 package be.faros.springbootspc.services;
 
 import be.faros.springbootspc.model.Pet;
+
 import java.util.Set;
 
 public interface PetService {
-
     Pet findById(Long id);
     Pet save(Pet pet);
     Set<Pet> findAll();
