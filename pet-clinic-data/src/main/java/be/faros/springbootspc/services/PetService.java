234bb193737b926d18641b8091cdc18d315e8550
Lennart Cockx <lennart.cockx@gmail.com>
2018-12-18 10:40:17 +0100

    created common CRUD interface with generics for most methods closes #12

--- pet-clinic-data/src/main/java/be/faros/springbootspc/services/PetService.java
+++ pet-clinic-data/src/main/java/be/faros/springbootspc/services/PetService.java
@@ -2,10 +2,5 @@ package be.faros.springbootspc.services;
 
 import be.faros.springbootspc.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet,Long> {
 }
--- pet-clinic-data/src/main/java/be/faros/springbootspc/services/VetService.java
+++ pet-clinic-data/src/main/java/be/faros/springbootspc/services/VetService.java
@@ -2,11 +2,6 @@ package be.faros.springbootspc.services;
 
 import be.faros.springbootspc.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
+public interface VetService extends CrudService<Vet,Long> {
     Vet findByLastName(String lastName);
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
