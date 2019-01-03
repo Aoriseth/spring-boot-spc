4256c912aab29c22e0027bda82d8bc76a83581f8
Lennart Cockx <lennart.cockx@gmail.com>
2019-01-03 11:32:32 +0100

    Added bootstrap data for PetTypes Closes #34

--- pet-clinic-web/src/main/java/be/faros/springbootspc/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/be/faros/springbootspc/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package be.faros.springbootspc.bootstrap;
 
 import be.faros.springbootspc.model.Owner;
+import be.faros.springbootspc.model.PetType;
 import be.faros.springbootspc.model.Vet;
 import be.faros.springbootspc.services.OwnerService;
+import be.faros.springbootspc.services.PetTypeService;
 import be.faros.springbootspc.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,19 +16,31 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petService;
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petService1) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petService = petService1;
     }
 
     @Override
     public void run(String... args) {
 
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        petService.save(dog);
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        petService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("John");
         owner1.setLastName("Machigan");
+        owner1.setAddress("Maridian Lane 15");
+        owner1.setCity("Goro City");
+        owner1.setTelephone("0148478573");
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Mike");
