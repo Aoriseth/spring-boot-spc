5aec4fcf422cfc8f953b2969899b2f13cc767410
Lennart Cockx <lennart.cockx@gmail.com>
2019-01-08 11:34:53 +0100

    Created Service implementation of SpecialtyRepository using JPA. Closes #46

---
+++ pet-clinic-data/src/main/java/be/faros/springbootspc/services/JPA/SpecialtyJPAService.java
@@ -0,0 +1,46 @@
+package be.faros.springbootspc.services.JPA;
+
+import be.faros.springbootspc.model.Specialty;
+import be.faros.springbootspc.repositories.SpecialtyRepository;
+import be.faros.springbootspc.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+
+@Service
+@Profile("springdatajpa")
+public class SpecialtyJPAService implements SpecialtyService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtyJPAService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        return specialtyRepository.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return specialtyRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty specialty) {
+        return specialtyRepository.save(specialty);
+    }
+
+    @Override
+    public void delete(Specialty specialty) {
+        specialtyRepository.delete(specialty);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialtyRepository.deleteById(id);
+    }
+}
