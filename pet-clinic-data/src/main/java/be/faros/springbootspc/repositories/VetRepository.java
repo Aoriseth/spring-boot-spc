6e4b74ad8088f9bcbc2320f24dd633addaafad46
Lennart Cockx <lennart.cockx@gmail.com>
2019-01-08 11:24:26 +0100

    Created Service implementation of VetRepository using JPA. Closes #43

--- pet-clinic-data/src/main/java/be/faros/springbootspc/repositories/VetRepository.java
+++ pet-clinic-data/src/main/java/be/faros/springbootspc/repositories/VetRepository.java
@@ -3,5 +3,9 @@ package be.faros.springbootspc.repositories;
 import be.faros.springbootspc.model.Vet;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 public interface VetRepository extends CrudRepository<Vet,Long> {
+    Vet findByLastName(String lastName);
+    Set<Vet> findAll();
 }
---
+++ pet-clinic-data/src/main/java/be/faros/springbootspc/services/JPA/VetJPAService.java
@@ -0,0 +1,50 @@
+package be.faros.springbootspc.services.JPA;
+
+import be.faros.springbootspc.model.Vet;
+import be.faros.springbootspc.repositories.VetRepository;
+import be.faros.springbootspc.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetJPAService implements VetService {
+    private final VetRepository vetRepository;
+
+    public VetJPAService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+
+    @Override
+    public Vet findByLastName(String lastName) {
+        return vetRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return vetRepository.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        vetRepository.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
