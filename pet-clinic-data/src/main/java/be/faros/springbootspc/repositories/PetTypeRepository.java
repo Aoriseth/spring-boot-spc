46dc9049a8bfd990ff2e5d29ec5adf541b79d4fc
Lennart Cockx <lennart.cockx@gmail.com>
2019-01-08 11:28:27 +0100

    Created Service implementation of PetTypeRepository using JPA. Closes #44

--- pet-clinic-data/src/main/java/be/faros/springbootspc/repositories/PetTypeRepository.java
+++ pet-clinic-data/src/main/java/be/faros/springbootspc/repositories/PetTypeRepository.java
@@ -3,5 +3,8 @@ package be.faros.springbootspc.repositories;
 import be.faros.springbootspc.model.PetType;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 public interface PetTypeRepository extends CrudRepository<PetType,Long> {
+    Set<PetType> findAll();
 }
